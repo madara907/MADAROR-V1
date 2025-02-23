25cac40387b28c4dada64b5c6895d743d017c178
madara907 <freefireeurope847@gmail.com>
2025-02-23 17:48:09 +0100

    Create confi.js

---
+++ confi.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "5ZgFUJQQ#N7RsQyvP8JevgFfma0Bq4XQTd5FzQ1ocZDLAKNHc7CU", 
+MONGODB: process.env.MONGODB || "mongodb+srv://raizen:darki123@cluster0.utsln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
+};
+//maitez la session du ruok md ici
